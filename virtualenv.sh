d4f2950dc2eae27c88ff8b2192f2b1e5db8c8937
John Brodie <jdb356@drexel.edu>
2012-10-28 15:34:22 -0400

    auto patch Flask-SQLAlchemy

--- virtualenv.sh
+++ virtualenv.sh
@@ -5,4 +5,5 @@ virtualenv --distribute .;
 source bin/activate;
 pip install --upgrade distribute
 pip install -r requirements.txt;
+patch lib/python2.7/site-packages/flask_sqlalchemy.py < patch_flask_sqlA.patch
 nosetests -x
