e43de0df4752a5748f627d8ac594eefa59ba25b8
John Brodie <jdb356@drexel.edu>
2012-10-28 00:26:29 -0400

    Add logging

--- whatup_api/hello.py
+++ whatup_api/hello.py
@@ -1,5 +1,9 @@
 """Hello world example"""
 import config
+import logging
+import logging.config
+
+from os import environ
 
 from flask import Flask
 from flask.ext.restless import APIManager
@@ -7,7 +11,19 @@ from flask.ext.restless import APIManager
 from whatup_api import models as m
 
 app = Flask(__name__)
+
+# Load config, overwrite config with values from file
+# specified as env var, if set.
 app.config.from_object(config)
+env_var = environ.get('WHATUPCONFIG')
+if env_var:
+    app.config.from_envvar('WHATUPCONFIG')
+
+# Set up logging
+logging.config.fileConfig('../../setup.cfg')
+log = logging.getLogger('whatupAPI')
+app.logger.addHandler(log)
+
 db = m.init_app(app)
 
 manager = APIManager(app, flask_sqlalchemy_db=db)
