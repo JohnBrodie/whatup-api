b94d2231409ae8bfd547d4a5bd7142c6faf24556
John Brodie <jdb356@drexel.edu>
2012-10-29 00:56:05 -0400

    refactor default > Default

--- whatup_api/tests/unit/models/test_subscription.py
+++ whatup_api/tests/unit/models/test_subscription.py
@@ -10,21 +10,21 @@ class SubscriptionModelTestCase(ModelTestCase):
     def setUp(self):
         super(SubscriptionModelTestCase, self).setUp()
         self.subscription = self.db.session.query(m.Subscription) \
-            .filter_by(id=self.subscription_data.default.id).one()
+            .filter_by(id=self.subscription_data.Default.id).one()
 
     def tearDown(self):
         super(SubscriptionModelTestCase, self).tearDown()
 
     def should_have_id(self):
-        self.assertEquals(self.subscription.id, self.subscription_data.default.id)
+        self.assertEquals(self.subscription.id, self.subscription_data.Default.id)
 
     def should_have_created_at(self):
         self.assertEquals(self.subscription.created_at,
-                          self.subscription_data.default.created_at)
+                          self.subscription_data.Default.created_at)
 
     def should_have_modified_at(self):
         self.assertEquals(self.subscription.modified_at,
-                          self.subscription_data.default.modified_at)
+                          self.subscription_data.Default.modified_at)
 
     def should_have_user(self):
-        self.assertEquals(self.subscription.owner.id, self.user_data.default.id)
+        self.assertEquals(self.subscription.owner.id, self.user_data.Default.id)
