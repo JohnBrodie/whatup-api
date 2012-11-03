bb338cefcebe823e095bcb38f8617c555dcd304a
noah-black <noahblack@gmail.com>
2012-11-03 15:13:32 -0400

    Fixed Post model's 'tags' relationship

--- whatup_api/models/post.py
+++ whatup_api/models/post.py
@@ -2,6 +2,8 @@
 
 from whatup_api.models import db
 
+from sqlalchemy.ext.associationproxy import association_proxy
+
 postTags = db.Table('posttags', db.metadata,
     db.Column('post', db.Integer, db.ForeignKey('posts.id')),
     db.Column('tag', db.Integer, db.ForeignKey('tags.id'))
@@ -19,8 +21,8 @@ class Post(db.Model):
     topic = db.Column(db.String(1000))
     body = db.Column(db.String(1000))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    tags = db.relationship("Tag",
-        secondary=postTags, lazy='dynamic')
+    tags = db.relationship("Tag", secondary=lambda: postTags)
+    tag_names = association_proxy('tags', 'name')
     # TODO TAGS
     # TODO REFERENCES
     # TODO ATTACHMENTS
