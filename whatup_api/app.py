a7f6586f213a8ad8e406c6cc05a985c09ba3c6b0
John Brodie <jdb356@drexel.edu>
2012-11-04 17:11:57 -0500

    monkey patch gevent logging

--- whatup_api/app.py
+++ whatup_api/app.py
@@ -1,10 +1,24 @@
-# TODO quick  hack that will go away
 import os
 import sys
 sys.path.append(os.path.abspath('.'))
 
+import gevent
 from gevent.wsgi import WSGIServer
-from whatup_api.hello import app
+from whatup_api.hello import app, log
 
-http_server = WSGIServer(('', 5000), app)
+
+# Until the next version of gevent comes out,
+# we need to monkey-patch log_request to make
+# it work with Logger objects like Flask uses.
+def log_request(self, len):
+    log = self.server.log
+    if log:
+        if hasattr(log, "info"):
+            log.info(self.format_request() + '\n')
+        else:
+            log.write(self.format_request() + '\n')
+
+gevent.wsgi.WSGIHandler.log_request = log_request
+
+http_server = WSGIServer(('', 5000), app, log=log)
 http_server.serve_forever()
