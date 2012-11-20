a4ff39d87080657ea35c72ecefa9773bf3d7ab20
John Brodie <jdb356@drexel.edu>
2012-11-19 21:54:53 -0500

    Fix PEP8 in tests, remove prints

--- whatup_api/tests/unit/models/test_subscription.py
+++ whatup_api/tests/unit/models/test_subscription.py
@@ -1,10 +1,7 @@
 """Test case for Subscription model"""
 import whatup_api.models as m
-from sqlalchemy.exc import IntegrityError
 
 from whatup_api.tests.unit.models import ModelTestCase
-
-
 
 
 class SubscriptionModelTestCase(ModelTestCase):
@@ -23,12 +20,14 @@ class SubscriptionModelTestCase(ModelTestCase):
     def tearDown(self):
         super(SubscriptionModelTestCase, self).tearDown()
 
+
 class DescribeSubscriptionModel(SubscriptionModelTestCase):
     def should_have_table_name(self):
         self.assertEquals(m.Subscription.__tablename__, 'subscriptions')
 
     def should_have_id_as_pkey(self):
         self.assertTrue(self.is_primary_key('id'))
+
 
 class DescribeIdColumn(SubscriptionModelTestCase):
     def should_have_id(self):
@@ -37,6 +36,7 @@ class DescribeIdColumn(SubscriptionModelTestCase):
 
     def should_have_id_as_integer(self):
         self.assertTrue(self.is_type('id', self.db.Integer))
+
 
 class DescribeCreatedAtColumn(SubscriptionModelTestCase):
     def should_have_default_created_at(self):
@@ -47,6 +47,7 @@ class DescribeCreatedAtColumn(SubscriptionModelTestCase):
 
     def should_have_non_nullable_created_at(self):
         self.assertFalse(self.is_nullable('created_at'))
+
 
 class DescribeModifiedAtColumn(SubscriptionModelTestCase):
     def should_have_modified_at(self):
@@ -62,23 +63,28 @@ class DescribeModifiedAtColumn(SubscriptionModelTestCase):
     def should_have_default_modified_at(self):
         self.assertTrue(self.compare_time(self.specifies_none.modified_at))
 
+
 class DescribeUserIdColumn(SubscriptionModelTestCase):
     def should_have_user_id(self):
-        self.assertEquals(self.subscription.user_id, self.subscription_data.Default.user_id)
+        self.assertEquals(self.subscription.user_id,
+                          self.subscription_data.Default.user_id)
 
     def should_have_user_id_as_integer(self):
         self.assertTrue(self.is_type('user_id', self.db.Integer))
+
 
 class DescribeOwnerRelationship(SubscriptionModelTestCase):
     def should_have_owner(self):
         owner = self.subscription.owner
         self.assertEqual(owner.id, self.user_data.Default.id)
 
+
 class DescribeTagRelationship(SubscriptionModelTestCase):
     def should_have_tags(self):
         tags = self.subscription.tags.all()
         for tag in tags:
             self.assertEqual(tag.author.id, self.user.id)
+
     def should_have_tags_relation_to_tags_model(self):
         self.assertEquals(self.has_target('tags'), 'tags')
 
@@ -86,8 +92,8 @@ class DescribeTagRelationship(SubscriptionModelTestCase):
         self.assertEquals(self.is_lazy('tags'), 'dynamic')
 
     def should_have_tags_secondary_table(self):
-        print self.has_secondary('tags')
         self.assertEquals(self.has_secondary('tags'), 'substags')
+
 
 class DescribeTagNamesAssociationProxy(SubscriptionModelTestCase):
     def should_be_able_to_add_tag(self):
@@ -97,11 +103,13 @@ class DescribeTagNamesAssociationProxy(SubscriptionModelTestCase):
         self.assertEquals(self.subscription.tag_names[0], 'test-tag1')
         self.assertEquals(self.subscription.tag_names[1], 'test-tag2')
 
+
 class DescribeUsersRelationship(SubscriptionModelTestCase):
     def should_have_users_relationship(self):
         users = self.subscription.users.all()
         for user in users:
             self.assertEqual(user.id, self.user.id)
+
     def should_have_users_relation_to_users_model(self):
         self.assertEquals(self.has_target('users'), 'users')
 
@@ -109,8 +117,8 @@ class DescribeUsersRelationship(SubscriptionModelTestCase):
         self.assertEquals(self.is_lazy('users'), 'dynamic')
 
     def should_have_users_secondary_table(self):
-        print self.has_secondary('users')
         self.assertEquals(self.has_secondary('users'), 'substags')
+
 
 class DescribeTagNamesAssociationProxy(SubscriptionModelTestCase):
     def should_be_able_to_add_tag(self):
