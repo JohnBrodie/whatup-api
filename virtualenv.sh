2ae396105a6af83e0d1cac8239d51a476c2d3f05
root <root@noah.(none)>
2012-12-02 03:27:41 -0500

    Patched RESTless PUT/relationship issue

--- virtualenv.sh
+++ virtualenv.sh
@@ -6,6 +6,7 @@ source bin/activate;
 pip install --upgrade distribute
 pip install -r requirements.txt;
 patch lib/python2.7/site-packages/flask_sqlalchemy.py < patch_flask_sqlA.patch
+patch lib/python2.7/site-packages/flask_restless/views.py < patch_flask_restless.patch
 nosetests --with-xunit
 if [[ $EUID -eq 107 ]]; then
     pkill -f 'python whatup_api/app.py'
