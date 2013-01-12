d9323240030c29dbcfacc5378890be1f78da2303
noah-black <noahblack@gmail.com>
2013-01-12 03:54:24 -0500

    Cleaned up attachments code

--- whatup_api/config.py
+++ whatup_api/config.py
@@ -2,4 +2,4 @@ DEBUG = True
 TESTING = True
 SQLALCHEMY_DATABASE_URI = 'mysql://root:whatup@localhost/tests'
 ATTACHMENTS_DIR = 'attachments'
-MAX_CONTENT_LENGTH = 10 * 1024 * 1024 # 100 mb
+MAX_CONTENT_LENGTH = 100 * 1024 * 1024 # 100 mb
