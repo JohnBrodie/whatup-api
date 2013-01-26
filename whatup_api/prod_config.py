fb3d4019a02b95c287395538634240dd01d83259
John Brodie <jdb356@drexel.edu>
2013-01-25 22:34:27 -0500

    Add secret key to configs

--- whatup_api/prod_config.py
+++ whatup_api/prod_config.py
@@ -2,4 +2,5 @@ DEBUG = False
 TESTING = False
 SQLALCHEMY_DATABASE_URI = 'mysql://dev:TheOfficeUs@whatupdb.cfan7xdzmwkp.us-east-1.rds.amazonaws.com/app'
 ATTACHMENTS_DIR = '/var/www/assets/files'
-MAX_CONTENT_LENGTH = 100 * 1024 * 1024 # 100 mb
+MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100 mb
+SECRET_KEY = 'MoarLikePublicKeyAmirite?'
