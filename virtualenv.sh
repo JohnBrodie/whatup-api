a2c39804b29cf7d04b0c09160db936cf932f7bcf
John Brodie <jdb356@drexel.edu>
2013-02-10 16:11:19 -0500

    deploy via apache

--- virtualenv.sh
+++ virtualenv.sh
@@ -9,6 +9,5 @@ patch lib/python2.7/site-packages/flask_sqlalchemy.py < patch_flask_sqlA.patch
 patch -p0 -i patch_flask_restless.patch
 make coverage
 if [[ $EUID -eq 107 ]]; then
-    pkill -f 'python whatup_api/app.py'
-    BUILD_ID=dontKillMe python whatup_api/app.py &
+    touch /var/www/api/whatup_api.wsgi  # Tell apache to reload app
 fi
