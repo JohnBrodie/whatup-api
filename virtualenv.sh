4fe14c496cca114c041daf2c1da7c1b19c408554
John Brodie <jdb356@drexel.edu>
2013-02-17 15:29:33 -0500

    logging: change staging logging

--- virtualenv.sh
+++ virtualenv.sh
@@ -15,6 +15,7 @@ if [ "$1" -eq 0 ]; then # Production
     fi
 else # Staging
     mv whatup_api/staging_config.py whatup_api/prod_config.py
+    mv staging_setup.cfg setup.cfg
     if [[ $EUID -eq 107 ]]; then
         touch /var/www/s-api/whatup_api.wsgi  # Tell apache to reload app
     fi
