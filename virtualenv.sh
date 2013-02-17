b972419cfb01142bafdf9c5b0df50e39623c419a
John Brodie <jdb356@drexel.edu>
2013-02-17 15:11:15 -0500

    virtualenv: fix common tasks

--- virtualenv.sh
+++ virtualenv.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+mysql -uroot -pwhatup -e "drop database tests; create database tests";
+virtualenv --distribute .;
+source bin/activate;
+pip install --use-mirrors --upgrade distribute
+pip install --use-mirrors -r requirements.txt;
+patch lib/python2.7/site-packages/flask_sqlalchemy.py < patch_flask_sqlA.patch
+patch -p0 -i patch_flask_restless.patch
+make coverage
+
 if [ "$1" -eq 0 ]; then # Production
     if [[ $EUID -eq 107 ]]; then
         touch /var/www/api/whatup_api.wsgi  # Tell apache to reload app
