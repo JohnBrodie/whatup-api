4a92725b7a4aaed64b37481c4c4ace5e97af7887
John Brodie <jdb356@drexel.edu>
2013-02-17 14:57:37 -0500

    config: Add staging config

---
+++ whatup_api/staging_config.py
@@ -0,0 +1,6 @@
+DEBUG = False
+TESTING = False
+SQLALCHEMY_DATABASE_URI = 'mysql://dev:TheOfficeUs@whatupdb.cfan7xdzmwkp.us-east-1.rds.amazonaws.com/staging'
+ATTACHMENTS_DIR = '/var/www/assets/files'
+MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100 mb
+SECRET_KEY = 'MoarLikePublicKeyAmirite?'
