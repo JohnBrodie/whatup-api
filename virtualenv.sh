f6e15ec38b65316413b272170935202a292e26f8
John Brodie <jdb356@drexel.edu>
2013-02-17 14:58:00 -0500

    virtualenv: Accept staging param

--- virtualenv.sh
+++ virtualenv.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-mysql -uroot -pwhatup -e "drop database tests; create database tests";
-virtualenv --distribute .;
-source bin/activate;
-pip install --use-mirrors --upgrade distribute
-pip install --use-mirrors -r requirements.txt;
-patch lib/python2.7/site-packages/flask_sqlalchemy.py < patch_flask_sqlA.patch
-patch -p0 -i patch_flask_restless.patch
-make coverage
-if [[ $EUID -eq 107 ]]; then
-    touch /var/www/api/whatup_api.wsgi  # Tell apache to reload app
+if [ "$1" -eq 0 ]; then # Production
+    if [[ $EUID -eq 107 ]]; then
+        touch /var/www/api/whatup_api.wsgi  # Tell apache to reload app
+    fi
+else # Staging
+    mv whatup_api/staging_config.py whatup_api/prod_config.py
+    if [[ $EUID -eq 107 ]]; then
+        touch /var/www/s-api/whatup_api.wsgi  # Tell apache to reload app
+    fi
 fi
