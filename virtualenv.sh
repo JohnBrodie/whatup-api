ab8b659eb6a56f8b4a702a30de4444c665bddea0
John Brodie <jdb356@drexel.edu>
2013-03-07 14:40:56 -0500

    change euid to dev user

--- virtualenv.sh
+++ virtualenv.sh
@@ -10,12 +10,12 @@ patch -p0 -i patch_flask_restless.patch
 make coverage
 
 if [ "$1" -eq 0 ]; then # Production
-    if [[ $EUID -eq 107 ]]; then
+    if [[ $EUID -eq 1001 ]]; then
         find . -path './whatup_api/*.pyc' -delete
         touch /var/www/api/whatup_api.wsgi  # Tell apache to reload app
     fi
 else # Staging
-    if [[ $EUID -eq 107 ]]; then
+    if [[ $EUID -eq 1001 ]]; then
         mv whatup_api/staging_config.py whatup_api/prod_config.py
         mv staging_setup.cfg setup.cfg
         find . -path './whatup_api/*.pyc' -delete
