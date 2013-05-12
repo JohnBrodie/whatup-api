7828684307df45066af2769d39835756e5a0d80d
noah-black <noahblack@gmail.com>
2013-05-12 19:37:53 -0400

    added missing config field

--- whatup_api/staging_config.py
+++ whatup_api/staging_config.py
@@ -4,4 +4,5 @@ SQLALCHEMY_DATABASE_URI = 'mysql://dev:TheOfficeUs@whatupdb.cfan7xdzmwkp.us-east
 ATTACHMENTS_DIR = '/var/www/assets/files'
 MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100 mb
 SECRET_KEY = 'MoarLikePublicKeyAmirite?'
+MIN_PASSWORD_LENGTH = 8
 PAGE_LENGTH = 20
